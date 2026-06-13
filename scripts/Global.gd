dce6d9cc2285828bc924f3ed0862f7d01627f498
membercatcousin <membercatcousin@gmail.com>
2026-06-13 11:00:43 +0100

    update THE ENTIRE GAME

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -6,7 +6,18 @@ var debug: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-
+	# Application ID
+	DiscordRPC.app_id = 1515102497604112424
+	# this is boolean if everything worked
+	print("Discord working: " + str(DiscordRPC.get_is_discord_working()))
+	# Set the first custom text row of the activity here
+	DiscordRPC.details = "Playing unnamed block game!"
+	# Image key for small image from "Art Assets" from the Discord Developer website
+	DiscordRPC.large_image = "game"
+	# Tooltip text for the large image
+	DiscordRPC.large_image_text = "game"
+	# Always refresh after changing the values!
+	DiscordRPC.refresh()
 	pass # Replace with function body.
 
 
